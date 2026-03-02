bb8c4e529949263f58b70b9f5365e87e28370991
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-02 21:22:39 +1100

    Add initial logic structure

---
+++ main.py
@@ -0,0 +1,31 @@
+from langchain_core.prompts import ChatPromptTemplate
+import config
+import document
+import llm
+
+def main():
+    document_context = document.curate_context(document.get_documents(config.CONTEXT_DIR))
+    
+    prompt = ChatPromptTemplate.from_template("""
+    You are a helpful AI researcher. Give key insights from the provided context. Limit insights to 5 main points. Ensure to provide paper tittles and authors for each insight.
+
+    <context>
+    {context}
+    </context>
+
+    Question: {question}
+    """)
+    
+    model = llm.get_chat_model(config.CHAT_MODEL)
+    
+    chain = prompt | model
+    
+    response = chain.invoke({
+        "context": document_context,
+        "question": "What are the key insights from the provided research papers?"
+    })
+    
+    print(response.content)
+    
+if __name__ == "__main__":
+    main()
