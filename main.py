b36d4fb135ea3e9d30a9d8d62ad641d7a4a1d5af
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-03 20:44:19 +1100

    Refactor LLM prompt

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     document_context = document.curate_context(document.get_documents(config.CONTEXT_DIR))
     
     prompt = ChatPromptTemplate.from_template("""
-    You are a helpful AI researcher. Give key insights from the provided context. Limit insights to 5 main points. Ensure to provide paper tittles and authors for each insight.
+    You are a helpful AI researcher. Give key insights from the provided context. Limit insights to 1 point per paper. Ensure to provide paper tittles and authors for each insight.
 
     <context>
     {context}
@@ -22,7 +22,7 @@ def main():
     
     response = chain.invoke({
         "context": document_context,
-        "question": "What are the key insights from the provided research papers?"
+        "question": "Summarise the attached papers."
     })
     
     print(response.content)
